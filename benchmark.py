1b0aa64aa54d60d9aef87285bc01cb69adc7bd88
Pedro Sorge <pedrosorge2005@gmail.com>
2026-09-09 10:41:53 -0300

    Adding graph generation modules

--- benchmark.py
+++ benchmark.py
@@ -8,6 +8,62 @@ import time
 
 SEED = 2027
 
+# Gera grafo esparso 
+def gen_sparse_random(n : int, rng : random.Random) -> list[list]: 
+
+    # Em grafos esparsos é esperado que m ~ n. Usaremas m = 4n como alvo 
+    m_target = 4 * n # número de arestas que queremos que o grafo tenha
+    graph = [[] for _ in range(n)] # grafo
+    edges = set() # conjunt de vertices já conectados
+    m_count = 0 # contador de arestas criadas
+
+    while len(edges) < m_target and m_count < m_target * 20:
+        u = rng.randrange(n) 
+        v = rng.randrange(n)
+        m_count += 1
+        if u == v or (u, v) in edges:
+            continue
+        edges.add((u, v))
+        graph[u].append((v, rng.uniform(1, 100)))
+
+    return graph
+
+# Gera grafo denso
+def gen_dense_random(n: int, rng : random.Random) -> list[list]:
+
+    # Em grafos esparsos é esperado que m ~ n^2.
+    graph = [[] for _ in range(n)]  
+
+    # Conectamos todos os nós entre todos os nós
+    for i in range(n):
+        for j in range(n):
+
+            if(i == j):
+                continue
+            
+            graph[i].append((j, rng.uniform(1,100)))
+
+    return graph
+
+def gen_dijkstra_ladder(n :int):
+
+    base_weight = n * 2
+
+    grafo = [[] for _ in range(n)]
+    
+    for i in range(n-1):
+        grafo[i].append((i+1, base_weight))
+    
+    for i in range(n - 2):
+        for j in range(i + 2, n):
+            w = (j - i) * base_weight - 1
+            grafo[i].append((j, w))
+            
+    return grafo
+
+
+
+
 
 def medir(func, repeticoes: int = 7) -> tuple[float, float]:
     """Retorne mediana e desvio absoluto mediano, em segundos."""
